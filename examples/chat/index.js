9fd1e4b12e04e738933b5fd51574e019c3237f2f
Mannar Hielal <mannar.hielal@liip.ch>
2019-09-04 10:02:17 +0200

    initial commit

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -4,7 +4,7 @@ var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('../..')(server);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3002;
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
